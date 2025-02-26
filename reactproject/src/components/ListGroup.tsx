9f1181de612aafb5acf7cd8db138746ec1a94a9e
hum39k <2162923@student.uma.pt>
2025-02-26 18:48:34 +0000

    Update ListGroup.tsx

--- reactproject/src/components/ListGroup.tsx
+++ reactproject/src/components/ListGroup.tsx
@@ -1,19 +1,24 @@
-import { Fragment }  from "react/jsx-runtime";
+
 
 const localidades = ["Funchal","Santa Cruz", "Ribeira Brava", "Calheta", "Porto Moniz"];
 
 
 
 function ListGroup(){
+    if (localidades.length == 0)
+        return <h1>Array está vazio!</h1>
+    
+    // return localidades.length === 0 ? <h1>Array Vazio</h1> : null
+
     return(
-<Fragment>
+<>
 <h1>Titulo da lista</h1>
 <ul className="list-group">
     {
     localidades.map((item) => (<li className="list-group-item" key={item}>{item}</li>))
     }
 </ul>
-</Fragment>
+</>
 
     );
 
